f577891c536707f7e5d227ba3baa4c5bddfc4328
Yvan Menuet <yvan.menuet@wizeline.com>
2024-04-15 19:22:46 -0400

    Restructure backend repo, install Typescript and add JSON helpers.

---
+++ backend/src/utils/json-helpers.ts
@@ -0,0 +1,93 @@
+const fs = require("fs");
+
+const getPath = (tableName: string) => {
+  const path = require("path");
+  return path.resolve(__dirname, `../../db/${tableName}.json`);
+};
+
+const readAllTable = async (tableName: string): Promise<any> => {
+  if (!tableName) {
+    throw new Error("tableName is required!");
+  }
+  return new Promise((resolve, reject) =>
+    fs.readFile(getPath(tableName), "utf8", (err: Error, data: any) => {
+      if (err) {
+        console.error(err);
+        reject(err);
+      }
+      resolve(JSON.parse(data));
+    })
+  );
+};
+
+const writeTable = async (tableName: string, data: any) => {
+  if (!tableName) {
+    throw new Error("tableName is required!");
+  }
+  if (!data) {
+    throw new Error("data is required!");
+  }
+  return new Promise((resolve, reject) =>
+    fs.writeFile(getPath(tableName), JSON.stringify(data), (err: Error) => {
+      if (err) {
+        console.error(err);
+        reject(err);
+      }
+      resolve(data);
+    })
+  );
+};
+
+const createElement = async (tableName: string, data: any) => {
+  if (!data) {
+    throw new Error("data is required!");
+  }
+  const allData = await readAllTable(tableName);
+  const id = Object.keys(allData).length + 1;
+  allData[id] = { ...data, id };
+  await writeTable(tableName, allData);
+  return allData[id];
+};
+
+const getAllTable = async (tableName: string) => {
+  const data = await readAllTable(tableName);
+  return Object.values(data);
+};
+
+const getElementById = async (tableName: string, id: string) => {
+  if (!id) {
+    throw new Error("id is required!");
+  }
+  const data = await readAllTable(tableName);
+  return data[id];
+};
+
+const updateElementById = async (tableName: string, id: string, data: any) => {
+  if (!id) {
+    throw new Error("id is required!");
+  }
+  if (!data) {
+    throw new Error("data is required!");
+  }
+  const allData = await readAllTable(tableName);
+  allData[id] = { ...allData[id], ...data };
+  await writeTable(tableName, allData);
+  return;
+};
+
+const deleteElementById = async (tableName: string, id: string) => {
+  if (!id) {
+    throw new Error("id is required!");
+  }
+  const allData = await readAllTable(tableName);
+  delete allData[id];
+  await writeTable(tableName, allData);
+  return;
+};
+
+module.exports = {
+  getElementById,
+  getAllTable,
+  updateElementById,
+  deleteElementById,
+};
